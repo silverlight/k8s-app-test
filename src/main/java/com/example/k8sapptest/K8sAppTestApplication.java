c75b2c73210540e75cdd2630f31a0a5da7ee5c84
Guang.Yang <aezio@qq.com>
2022-08-15 16:56:04 +0800

    Initial commit

---
+++ src/main/java/com/example/k8sapptest/K8sAppTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.k8sapptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sAppTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sAppTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/k8sapptest/K8sAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.k8sapptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sAppTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
